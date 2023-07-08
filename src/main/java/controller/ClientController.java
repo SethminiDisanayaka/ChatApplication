44080f9a18f8a1a19eff1fb927edc0f7b25b78f3
SethminiDissanayake <sethminidissa@gmail.com>
2023-07-08 18:10:19 +0530

    Login Form Controller Created,Client Form fxml created

---
+++ src/main/java/controller/ClientController.java
@@ -0,0 +1,47 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class ClientController {
+
+    @FXML
+    private AnchorPane ClientPane;
+
+    @FXML
+    private TextField txtMessage;
+
+    @FXML
+    void Mouse_On_Click_Hide_Emogi(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btn_image_on_action(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btn_imogi_on_action(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btn_logout_On_action(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btn_massage_send_on_action(MouseEvent event) {
+
+    }
+
+    @FXML
+    void msgOnAction(ActionEvent event) {
+
+    }
+
+}
