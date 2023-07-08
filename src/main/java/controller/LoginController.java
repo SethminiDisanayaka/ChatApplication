a08de7652dfb78e6ae5bd34eabb216ee2de957fc
SethminiDissanayake <sethminidissa@gmail.com>
2023-07-08 13:31:06 +0530

    LoginController Created

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -2,20 +2,29 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
 
     @FXML
-    private AnchorPane LoginPane;
-
-    @FXML
     private TextField txtname;
+    static String username;
 
     @FXML
-    void NameOnAction(ActionEvent event) {
-
+    void NameOnAction(ActionEvent event) throws IOException {
+        username=txtname.getText();
+        txtname.clear();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(LoginController.class.getResource("/view/client.fxml"))));
+        stage.close();
+        stage.centerOnScreen();
+        stage.show();
     }
 
 }
