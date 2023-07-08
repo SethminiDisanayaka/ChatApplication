d84d6634d2c7dd8563401e85cb574169d6cf0452
SethminiDissanayake <sethminidissa@gmail.com>
2023-07-08 12:49:13 +0530

    LoginForm.fxml created

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginController {
+
+    @FXML
+    private AnchorPane LoginPane;
+
+    @FXML
+    private TextField txtname;
+
+    @FXML
+    void NameOnAction(ActionEvent event) {
+
+    }
+
+}
